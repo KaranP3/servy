ad6d6977d6510624e20ef8506bc841e2bd495422
KaranP3 <karanpillai93@gmail.com>
2023-04-29 11:36:32 +0530

    Add advanced pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,7 +16,12 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
   def route(conv) do
@@ -24,26 +29,41 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{conv | resp_body: "Teddy, Smoky, Paddington"}
+    %{conv | status: 200, resp_body: "Teddy, Smoky, Paddington"}
   end
 
-  def route(conv, "GET", "/bigfoot") do
+  def route(conv, "GET", "/bears" <> id) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
 
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "No #{path} found"}
   end
 
   @spec format_response(atom | %{:resp_body => binary, optional(any) => any}) :: <<_::64, _::_*8>>
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: #{String.length(conv.resp_body)}"
+    Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "CREATED",
+      401 => "UNAUTHORIZED",
+      403 => "FORBIDDEN",
+      404 => "NOT FOUND",
+      500 => "INTERNAL SERVER ERROR"
+    }[code]
   end
 end
 
@@ -60,6 +80,18 @@ IO.puts(response)
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
